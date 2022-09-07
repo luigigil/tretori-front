919ddf27400e8e4eac362cc22e5461fad4c4b350
luigigil <lgil.ctr@touchbistro.com>
2022-09-07 17:39:39 -0400

    chore: linting project

--- src/components/pages/clients/physical-person/FormPhysicalPerson.tsx
+++ src/components/pages/clients/physical-person/FormPhysicalPerson.tsx
@@ -29,14 +29,14 @@ const schema = Joi.object({
   name: Joi.string().min(2).max(200).required(),
   cpf: Joi.string().min(11).max(11).required(),
   rg: Joi.string().max(20).required(),
-  rg_emissor: Joi.string().max(20).required(),
-  rg_emissor_uf: Joi.string().max(20).required(),
+  rgEmissor: Joi.string().max(20).required(),
+  rgEmissorUf: Joi.string().max(20).required(),
   phone: Joi.string().max(25).required(),
   email: Joi.string()
     .max(250)
     .email({ minDomainSegments: 2, tlds: { allow: false } })
     .required(),
-  phone_secondary: Joi.string().max(25).allow(null, ''),
+  phoneSecondary: Joi.string().max(25).allow(null, ''),
   cep: Joi.string().max(20).allow(null, ''),
   address: Joi.string().max(250).allow(null, ''),
   city: Joi.string().max(100).allow(null, ''),
