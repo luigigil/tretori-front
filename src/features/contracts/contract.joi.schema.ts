484a7aa796d73beb2c67a00098afa16e757953f7
luigigil <lgil.ctr@touchbistro.com>
2022-10-15 15:56:46 -0400

    fix: contracts schema

--- src/features/contracts/contract.joi.schema.ts
+++ src/features/contracts/contract.joi.schema.ts
@@ -9,11 +9,11 @@ export const contractSchema = Joi.object({
   type: Joi.string().min(2).max(200).required(),
   version: Joi.number().required(),
   number_of_lives: Joi.number().required(),
-  validity_start: Joi.any().required(),
-  validity_end: Joi.any().required(),
+  validity_start: Joi.string().required(),
+  validity_end: Joi.string().required(),
   validity_time: Joi.number().required(),
   inclusion_period: Joi.string().min(2).max(200).required(),
-  cutoff_date: Joi.string().min(2).max(200).required(),
+  cutoff_date: Joi.string().required(),
   email_on_insurancy: Joi.string()
     .max(250)
     .email({ minDomainSegments: 2, tlds: { allow: false } })
@@ -27,7 +27,7 @@ export const contractSchema = Joi.object({
   cost: Joi.number().required(),
   invoice_amount: Joi.number().required(),
   total_contract_value: Joi.number().required(),
-  first_invoice_date: Joi.string().min(2).max(200).required(),
+  first_invoice_date: Joi.string().required(),
   // form access
   login_client: Joi.string().min(2).max(200),
   login_tret: Joi.string().min(2).max(200),
