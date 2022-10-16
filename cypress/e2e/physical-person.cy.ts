4d6dfc528dfef04e0564f951ebd95e4068212e39
arthurpieri <services+github@arthurpieri.com>
2022-10-16 18:43:34 -0300

    test: show list of physical people

--- cypress/e2e/physical-person.cy.ts
+++ cypress/e2e/physical-person.cy.ts
@@ -1,7 +1,7 @@
 /* eslint-disable spaced-comment */
 /// <reference types="cypress" />
 
-import fixture from '../fixtures/physical_person.json'
+import fixture from '../fixtures/physical_people.json'
 import useSessionInterceptor from '../helpers/useSessionInterceptor'
 
 describe('Insurance Page', () => {
@@ -9,4 +9,28 @@ describe('Insurance Page', () => {
   beforeEach(() => {
     useSessionInterceptor()
   })
+
+  it.only('it should show a list of physical people', () => {
+    cy.intercept('GET', `${baseUrl}/physical-person`, { fixture: 'physical_people.json' }).as(
+      'getPhysicalPeople',
+    )
+    cy.visit('/physical-person')
+    cy.wait(['@getPhysicalPeople'])
+
+    cy.get('#0-name').should('have.text', fixture[0].name)
+    cy.get('#0-cpf').should('have.text', fixture[0].cpf)
+    cy.get('#0-rg').should('have.text', fixture[0].rg)
+
+    cy.get('#1-name').should('have.text', fixture[1].name)
+    cy.get('#1-cpf').should('have.text', fixture[1].cpf)
+    cy.get('#1-rg').should('have.text', fixture[1].rg)
+  })
+
+  it('', () => {})
+
+  it('', () => {})
+
+  it('', () => {})
+
+  it('', () => {})
 })
