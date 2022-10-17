08a4740c9099bbb2590c67036cf44d613a19f3eb
arthurpieri <services+github@arthurpieri.com>
2022-10-17 17:56:28 -0300

    test: create physical person

--- cypress/e2e/physical-person.cy.ts
+++ cypress/e2e/physical-person.cy.ts
@@ -10,7 +10,7 @@ describe('Insurance Page', () => {
     useSessionInterceptor()
   })
 
-  it.only('it should show a list of physical people', () => {
+  it('it should show a list of physical people', () => {
     cy.intercept('GET', `${baseUrl}/physical-person`, { fixture: 'physical_people.json' }).as(
       'getPhysicalPeople',
     )
@@ -26,7 +26,30 @@ describe('Insurance Page', () => {
     cy.get('#1-rg').should('have.text', fixture[1].rg)
   })
 
-  it('', () => {})
+  it('should create a new physical person', () => {
+    cy.intercept('GET', `${baseUrl}/physical-person`, { fixture: 'physical_people.json' }).as(
+      'getPhysicalPeople',
+    )
+    cy.intercept('POST', `${baseUrl}/physical-person`, { statusCode: 201 }).as('savePhysicalPerson')
+
+    cy.visit('/physical-person')
+    cy.wait(['@getPhysicalPeople'])
+
+    cy.get('#newBtn').click()
+
+    cy.get('[name="name"]').type('Joselito')
+    cy.get('[name="cpf"]').type('99988877766')
+    cy.get('[name="rg"]').type('9878987')
+    cy.get('[name="rg_emissor"]').type('SSP')
+    cy.get('[name="rg_emissor_uf"]').type('SP')
+    // cy.get('[name="birthdate"]').type('01022003')
+
+    cy.get('#saveBtn').click()
+
+    cy.wait(['@savePhysicalPerson'])
+
+    cy.get('#notistack-snackbar').should('have.text', 'Pessoa Física adicionada com sucesso')
+  })
 
   it('', () => {})
 
