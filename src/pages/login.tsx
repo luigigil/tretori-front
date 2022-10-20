3c605d933cbf3fc7f6ad0849081e9aaf60bd62ce
douglas.yokoyama <douglas.shiro@gmail.com>
2022-10-20 02:20:12 -0300

    fix: issue 24 use static login images

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -50,17 +50,23 @@ export default function SignInSide() {
     }
   }
 
+  const minImageRandom = 1
+  const maxImageRandom = 4
+  const loginImageRandom = Math.floor(Math.random() * (maxImageRandom + minImageRandom))
+    .toString()
+    .padStart(2, '0')
   return (
     <ThemeProvider theme={theme}>
       <Grid container component='main' sx={{ height: '100vh' }}>
         <CssBaseline />
         <Grid
+          id='login-image'
           item
           xs={false}
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
+            backgroundImage: `url(/images/login/login-${loginImageRandom}.png)`,
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
